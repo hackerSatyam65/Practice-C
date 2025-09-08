578d1b746abe968cf1cd394aec132350c9922860
Satyam Vivek <satyamvivek81@gmail.com>
2025-09-08 23:33:54 +0530

    check address
    check address

---
+++ pointers/checkaddress.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int a=5;
+    // int* p=&a;
+    printf("The address of a is %p\n",&a);
+    // printf("The address of a using pointer p is %p\n",p);
+    return 0;
+}
